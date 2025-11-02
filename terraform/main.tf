3f914a5a6a446d5c72badc8f0b0ec235bf2e0538
thesandx <kumarjha.sandeep@gmail.com>
2025-11-02 17:01:01 +0530

    added user to the gcp project

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,63 +17,23 @@ resource "google_project_iam_member" "data_pipeline_bq_data_viewer" {
   member  = "serviceAccount:${google_service_account.data_pipeline.email}"
 }
 
-# Create custom role for BigQuery without delete permissions
-resource "google_project_iam_custom_role" "bigquery_no_delete" {
-  role_id     = "bigqueryNoDelete"
-  title       = "BigQuery Admin No Delete"
-  description = "BigQuery Admin role without delete permissions"
-  permissions = [
-    "bigquery.datasets.create",
-    "bigquery.datasets.get",
-    "bigquery.datasets.update",
-    "bigquery.datasets.getIamPolicy",
-    "bigquery.datasets.setIamPolicy",
-    "bigquery.jobs.create",
-    "bigquery.jobs.get",
-    "bigquery.jobs.list",
-    "bigquery.jobs.update",
-    "bigquery.tables.create",
-    "bigquery.tables.get",
-    "bigquery.tables.getData",
-    "bigquery.tables.getIamPolicy",
-    "bigquery.tables.list",
-    "bigquery.tables.setIamPolicy",
-    "bigquery.tables.update",
-    "bigquery.tables.updateData"
-  ]
-}
-
-# Create custom role for Storage without delete permissions
-resource "google_project_iam_custom_role" "storage_no_delete" {
-  role_id     = "storageNoDelete"
-  title       = "Storage Admin No Delete"
-  description = "Storage Admin role without delete permissions"
-  permissions = [
-    "storage.buckets.create",
-    "storage.buckets.get",
-    "storage.buckets.getIamPolicy",
-    "storage.buckets.list",
-    "storage.buckets.setIamPolicy",
-    "storage.buckets.update",
-    "storage.objects.create",
-    "storage.objects.get",
-    "storage.objects.getIamPolicy",
-    "storage.objects.list",
-    "storage.objects.setIamPolicy",
-    "storage.objects.update"
-  ]
-}
-
-# Assign custom BigQuery role to the user
-resource "google_project_iam_member" "user_bq_no_delete" {
+# Assign BigQuery Data Editor role to the user (allows read/write but not delete)
+resource "google_project_iam_member" "user_bq_editor" {
   project = var.project_id
-  role    = google_project_iam_custom_role.bigquery_no_delete.id
-  member  = "user:nikhila.nethikunta@gamil.com"
+  role    = "roles/bigquery.dataEditor"
+  member  = "user:nikhila.nethikunta@gmail.com"
 }
 
-# Assign custom Storage role to the user
-resource "google_project_iam_member" "user_storage_no_delete" {
+# Assign BigQuery User role to the user (allows to run queries)
+resource "google_project_iam_member" "user_bq_user" {
   project = var.project_id
-  role    = google_project_iam_custom_role.storage_no_delete.id
-  member  = "user:nikhila.nethikunta@gamil.com"
+  role    = "roles/bigquery.user"
+  member  = "user:nikhila.nethikunta@gmail.com"
+}
+
+# Assign Storage Object User role to the user (allows read/write but not delete)
+resource "google_project_iam_member" "user_storage_object_user" {
+  project = var.project_id
+  role    = "roles/storage.objectUser"
+  member  = "user:nikhila.nethikunta@gmail.com"
 }
