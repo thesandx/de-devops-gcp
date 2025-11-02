1858b47248150d3db79418e026ae2dde11ee426c
thesandx <kumarjha.sandeep@gmail.com>
2025-11-02 16:48:24 +0530

    added service account details

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,79 @@
-resource "google_service_account" "ci" {
-  account_id   = "tf-ci-sa"
-  display_name = "Terraform CI Service Account"
+resource "google_service_account" "data_pipeline" {
+  account_id   = "de-devops-data-pipeline"
+  display_name = "Data Pipeline Service Account"
 }
 
-resource "google_storage_bucket" "tfstate_bucket" {
-  name          = var.bucket_name
-  location      = var.region
-  force_destroy = false
+# Assign BigQuery Job User role to the data pipeline service account
+resource "google_project_iam_member" "data_pipeline_bq_job_user" {
+  project = var.project_id
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.data_pipeline.email}"
 }
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = var.bigquery_dataset
-  location   = var.region
+# Assign BigQuery Data Viewer role to the data pipeline service account
+resource "google_project_iam_member" "data_pipeline_bq_data_viewer" {
+  project = var.project_id
+  role    = "roles/bigquery.dataViewer"
+  member  = "serviceAccount:${google_service_account.data_pipeline.email}"
+}
+
+# Create custom role for BigQuery without delete permissions
+resource "google_project_iam_custom_role" "bigquery_no_delete" {
+  role_id     = "bigqueryNoDelete"
+  title       = "BigQuery Admin No Delete"
+  description = "BigQuery Admin role without delete permissions"
+  permissions = [
+    "bigquery.datasets.create",
+    "bigquery.datasets.get",
+    "bigquery.datasets.update",
+    "bigquery.datasets.getIamPolicy",
+    "bigquery.datasets.setIamPolicy",
+    "bigquery.jobs.create",
+    "bigquery.jobs.get",
+    "bigquery.jobs.list",
+    "bigquery.jobs.update",
+    "bigquery.tables.create",
+    "bigquery.tables.get",
+    "bigquery.tables.getData",
+    "bigquery.tables.getIamPolicy",
+    "bigquery.tables.list",
+    "bigquery.tables.setIamPolicy",
+    "bigquery.tables.update",
+    "bigquery.tables.updateData"
+  ]
+}
+
+# Create custom role for Storage without delete permissions
+resource "google_project_iam_custom_role" "storage_no_delete" {
+  role_id     = "storageNoDelete"
+  title       = "Storage Admin No Delete"
+  description = "Storage Admin role without delete permissions"
+  permissions = [
+    "storage.buckets.create",
+    "storage.buckets.get",
+    "storage.buckets.getIamPolicy",
+    "storage.buckets.list",
+    "storage.buckets.setIamPolicy",
+    "storage.buckets.update",
+    "storage.objects.create",
+    "storage.objects.get",
+    "storage.objects.getIamPolicy",
+    "storage.objects.list",
+    "storage.objects.setIamPolicy",
+    "storage.objects.update"
+  ]
+}
+
+# Assign custom BigQuery role to the user
+resource "google_project_iam_member" "user_bq_no_delete" {
+  project = var.project_id
+  role    = google_project_iam_custom_role.bigquery_no_delete.id
+  member  = "user:nikhila.nethikunta@gamil.com"
+}
+
+# Assign custom Storage role to the user
+resource "google_project_iam_member" "user_storage_no_delete" {
+  project = var.project_id
+  role    = google_project_iam_custom_role.storage_no_delete.id
+  member  = "user:nikhila.nethikunta@gamil.com"
 }
